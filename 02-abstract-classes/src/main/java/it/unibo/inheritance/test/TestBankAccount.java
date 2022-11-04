bf9c066d8e0e62ccf6ac8aec5b75a4171955a156
pianinator <113355583+pianinator@users.noreply.github.com>
2022-11-04 13:28:51 +0000

    Deploying to solutions from @ APICe-at-DISI/exercises@2a8cf3c78fed0f203db3925dda84c66a895b0cfc 🚀

--- 02-abstract-classes/src/main/java/it/unibo/inheritance/test/TestBankAccount.java
+++ 02-abstract-classes/src/main/java/it/unibo/inheritance/test/TestBankAccount.java
@@ -2,7 +2,9 @@ package it.unibo.inheritance.test;
 
 import it.unibo.inheritance.api.AccountHolder;
 import it.unibo.inheritance.api.BankAccount;
+import it.unibo.inheritance.impl.ClassicBankAccount;
 import it.unibo.inheritance.impl.ExtendedStrictBankAccount;
+import it.unibo.inheritance.impl.RestrictedBankAccount;
 import it.unibo.inheritance.impl.SimpleBankAccount;
 
 public class TestBankAccount {
@@ -64,10 +66,10 @@ public class TestBankAccount {
         /*
          * Change the two accounts to verify that the behaviour of restricted and simple bank account are preserved
          */
-        final BankAccount restricted = null;
+        final BankAccount restricted = new RestrictedBankAccount(aRossi, 0);
         final BankAccount extendedStrict = new ExtendedStrictBankAccount(aBianchi, 0);
         final BankAccount simpleBankAccount = new SimpleBankAccount(aRossi, 0);
-        final BankAccount classicBankAccount = null;
+        final BankAccount classicBankAccount = new ClassicBankAccount(aBianchi, 0);
         requireAccountsToWorkTheSameWay(restricted, extendedStrict);
         requireAccountsToWorkTheSameWay(simpleBankAccount, classicBankAccount);
     }
